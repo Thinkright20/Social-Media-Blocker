e5162804f0376574cc521094442a6c2b61aa5246
Thinkright20 <84785278+Thinkright20@users.noreply.github.com>
2021-06-07 16:01:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+"https://www.tiktok.com/", "https://www.instagram.com/", 
+"https://www.facebook.com/" , "https://www.twitch.tv" , 
+"https://www.youtube.com/" , "https://www.messenger.com/" , 
+"https://www.snapchat.com/" , "https://www.pinterest.ca/" , 
+"https://www.reddit.com/" , "https://twitter.com/" , "https://discord.gg/"
